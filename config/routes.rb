1937a77d1eb6a9e684a0c04c8cbd8132724c6148
junpei-osano <osanojunpei@gmail.com>
2023-07-02 13:58:18 +0900

    Update:production.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  get '/api/articles/index', to: 'articles#index'
+  get '/', to 'articles#index'
   scope :api do
     resources :articles, only: [:create, :show, :update, :destroy], param: :slug
   end
